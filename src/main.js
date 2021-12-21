06ae1c0e27af2f0c3a2986ca19cd33b4e78996fc
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2021-12-21 23:28:32 +0000

    Added animations and JQuery
    Added animations to the login screen. Created a function to check logins.
    Added support for JQuery

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// This function checks the login credentials
+// It takes no input in
+// Will display an error is the credentials are wrong
+function login() {
+    
+    // Get the username input
+    var uname = $("#uname").val()
+
+    // Get the password input
+    var pword = $("#psw").val()
+
+    // Hash both.
+
+    // Check against credentials
+
+    // Animating out the login screen
+    $(".login_page").addClass("animate__animated animate__bounceOutUp")
+    
+
+    // $(".login_page").removeClass("animate__bounceOutUp");$(".login_page").addClass("animate__bounceInRight") <Log out animation
+    // ^ Ajay will fix this later
+    
+}
