a8098a0168df3bf4017046523041a5a62536b71c
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2021-12-30 00:07:14 +0000

    Added main dashboard and the animations

--- src/main.js
+++ src/main.js
@@ -14,10 +14,35 @@ function login() {
     // Check against credentials
 
     // Animating out the login screen
-    $(".login_page").addClass("animate__animated animate__bounceOutUp")
+    login_page = document.querySelector('.login_page')
+
+    login_page.classList.add("animate__animated", "animate__bounceOutUp")
+
+    login_page.addEventListener('animationend', () => {
+        home = document.querySelector('.home')
+        home.style.visibility = "visible"
+        home.classList.add("animate__animated", "animate__slideInLeft"); 
+
+    });
+    
     
 
     // $(".login_page").removeClass("animate__bounceOutUp");$(".login_page").addClass("animate__bounceInRight") <Log out animation
     // ^ Ajay will fix this later
     
+}
+
+function loaddrone() {
+    // load animations
+    content = document.querySelector('.contentbox')
+    
+    content.style.visibility = "visible"
+
+    content.classList.add("animate__animated","animate__slideInRight")
+
+    content.addEventListener('animationend', () => {
+        content.classList.remove("animate__animated","animate__slideInRight")
+    });
+
+
 }
